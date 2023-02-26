87cba824db9101e6390aa46f4c478f6252152892
phozzy <arthur.fayzullin@gmail.com>
2023-02-26 23:26:07 +0100

    Use toggle terminal plugin

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,16 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    size = function(term)
+      if term.direction == "horizontal" then
+        return vim.o.lines * 0.3
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+    end,
+  },
+  keys = {
+    { "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "horizontal terminal" },
+  },
+}
