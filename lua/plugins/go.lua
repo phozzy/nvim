580940169489941c869fdb170e71f05b2e5e6a3f
phozzy <arthur.fayzullin@gmail.com>
2023-03-07 09:35:36 +0100

    Add dependency plugin for go plugin

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,6 +1,13 @@
 return {
   {
+    "ray-x/guihua.lua",
+    build = "cd lua/fzy && make",
+  },
+  {
     "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+    },
     config = true,
     event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
