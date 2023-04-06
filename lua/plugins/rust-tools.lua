1f79d7c663716b5635eef0d2fec1100f7eab500d
phozzy <arthur.fayzullin@gmail.com>
2023-04-07 00:16:59 +0200

    Install crates module for Rust development

--- lua/plugins/rust-tools.lua
+++ lua/plugins/rust-tools.lua
@@ -3,4 +3,12 @@ return {
     "simrat39/rust-tools.nvim",
     config = true,
   },
+  {
+    "saecki/crates.nvim",
+    version = "v0.3.0",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = true,
+  },
 }
